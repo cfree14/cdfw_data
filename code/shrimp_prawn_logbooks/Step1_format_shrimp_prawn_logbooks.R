90fe37693c40790ed774969e3f31abcfd96bd524
cfree14 <cfree14@gmail.com>
2024-07-19 12:35:23 -0700

    lots of work

--- code/shrimp_prawn_logbooks/Step1_format_shrimp_prawn_logbooks.R
+++ code/shrimp_prawn_logbooks/Step1_format_shrimp_prawn_logbooks.R
@@ -21,13 +21,50 @@ data_orig <- read.csv(file.path(indir, "LogSummary_shrimp-prawn_2024.03.29_Free 
 spp_key_orig <- readRDS("data/public/cdfw_keys/processed/CDFW_species_key.Rds")
 
 # HIGH PRIORITY
-# Finalize GPS coordinates
-# Mark lat/longs in block (derive block id)
+# Convert LORAN to latlong
+# Mark GPS reliability
 
 # LOW PRIORITY
-# Ask about units for head rope length?
 # Fill in missing vessel names/ids from another dataset
 
+
+# Helper functions
+################################################################################
+
+# Function to calculate the decimal hour of a day
+time_vec <- c("18:15", "07:30", "12:45")
+convert_to_decimal_hour <- function(time_str) {
+  # Split the time string into hours and minutes
+  time_parts <- strsplit(time_str, ":")
+  
+  # Convert hours and minutes to numeric and calculate decimal hour for each time
+  decimal_hours <- sapply(time_parts, function(tp) {
+    hours <- as.numeric(tp[1])
+    minutes <- as.numeric(tp[2])
+    hours + minutes / 60
+  })
+  
+  return(decimal_hours)
+}
+convert_to_decimal_hour(time_vec)
+
+# Calculate durations
+hr1 <- 6.2; hr2 <- 12.5
+hr1 <- 23; hr2 <- 2
+calc_duration <- function(hr1, hr2){
+  
+  # Duration when hour2 is on the same day
+  duration1 <- (hr2 - hr1) * 60
+  
+  # Duration when hour2 is on the next day
+  duration2 <- (24-hr1) + hr2
+  
+  # Select the right one
+  out <- ifelse(hr2>hr1, duration1, duration2)
+  
+  return(out)
+  
+}
 
 # Format data
 ################################################################################
@@ -52,7 +89,7 @@ data <- data_orig %>%
          net_type=net_type_desc, 
          time_set=settime,
          time_up=uptime,
-         duration_min=totaltime,
+         duration_min_orig=totaltime,
          spp_code=species_code,
          species_orig=market_cat_desc,
          catch_lbs=totalpounds,
@@ -88,7 +125,7 @@ data <- data_orig %>%
   # Add species name
   left_join(spp_key_orig %>% select(spp_code_num, comm_name), by=c("spp_code"="spp_code_num")) %>% 
   rename(species=comm_name) %>% 
-  # Format dates,
+  # Format dates
   mutate(date_depart=lubridate::mdy(date_depart),
          date_return=lubridate::mdy(date_return),
          date_tow=lubridate::mdy(date_tow)) %>% 
@@ -96,6 +133,9 @@ data <- data_orig %>%
   mutate(year=ifelse(!is.na(date_tow), lubridate::year(date_tow),
                      ifelse(!is.na(date_return), lubridate::year(date_return), lubridate::year(date_depart))),
          year=ifelse(is.na(year), year_orig, year)) %>% 
+  # Fix a longitude
+  mutate(set_long_deg=ifelse(set_long_dec==11954.37, 119, set_long_deg),
+         set_long_dec=ifelse(set_long_dec==11954.37, 54.37, set_long_dec)) %>%  
   # Calculate lat/long
   mutate(lat_dd_set=set_lat_deg+set_lat_dec/60,
          lat_dd_up=up_lat_deg+up_lat_dec/60,
@@ -106,31 +146,41 @@ data <- data_orig %>%
          lat_dd_up=ifelse(lat_dd_up==0, NA, lat_dd_up),
          long_dd_set=ifelse(long_dd_set==0, NA, long_dd_set),
          long_dd_up=ifelse(long_dd_up==0, NA, long_dd_up)) %>% 
+  # Format durations
+  mutate(duration_min_orig=ifelse(duration_min_orig<0, NA, duration_min_orig)) %>% 
+  # Convert times
+  mutate(time_set_num=convert_to_decimal_hour(time_set),
+         time_up_num=convert_to_decimal_hour(time_up)) %>% 
+  # Calculate duration
+  mutate(duration_min=calc_duration(time_set_num, time_up_num),
+         duration_min=ifelse(is.na(duration_min), duration_min_orig, duration_min)) %>% 
+  # Fix tow numbers
+  # 1327: logbook #56621 - tow 6 (13:27)
+  # 1338: logbook #56648 - tow 5 (13:38), 
+  # 1339: logbook #56648 - tow 6 (15:27)
+  # 1420: logbook #12561 - tow 3 (14:20)
+  mutate(tow_number=case_when(tow_number==1327 ~ 6,
+                              tow_number==1338 ~ 5,
+                              tow_number==1339 ~ 6,
+                              tow_number==1420 ~ 3,
+                              T ~ tow_number)) %>% 
   # Arrange
   select(logbook_id, vessel_id, vessel, port_code, port, 
          year, year_orig, date_depart, date_return, 
-         date_tow, tow_number,
+         date_tow, tow_number, tow_id,
          block_id,
          lat_dd_set, long_dd_set,
          lat_dd_up, long_dd_up,
          depth_fa_set, depth_fa_up,
-         time_set, time_up, duration_min,
+         time_set, time_up, 
+         time_set_num, time_up_num,
+         duration_min_orig, duration_min,
          net_type, 
          spp_code,  species_orig, species,
          catch_lbs,
          comments1, comments2,
-         everything()) %>% 
-  # Remove useless lat/long columns
-  select(-c(set_lat_deg, set_lat_dec,
-            set_long_deg, set_long_dec,
-            up_lat_deg, up_lat_dec,
-            up_long_deg, up_long_dec)) %>% 
-  # Remove these columns because they are empty
-  select(-c(set_loran_amin, set_loran_amax, up_loran_amin, up_loran_amax))
+         everything()) 
 
-# Inspect
-str(data)
-freeR::complete(data)
 
 # Inspect
 str(data)
@@ -142,6 +192,7 @@ spp_key <- data %>%
 freeR::which_duplicated(spp_key$spp_code)
 
 # Vessel
+# Danita does not have a vessel id
 vessel_key <- data %>% 
   count(vessel_id, vessel)
 freeR::which_duplicated(vessel_key$vessel_id)
@@ -163,11 +214,22 @@ table(data$block_id)
 # Net type
 table(data$net_type)
 
+# Tow number
+table(data$tow_number)
+
+# Time key
+time_key_set <- data %>% 
+  count(time_set, time_set_num)
+time_key_up <- data %>% 
+  count(time_up, time_up_num)
+
+# Duration check
+plot(duration_min ~ duration_min_orig, data)
+
 # GPS key
 gps_key <- data %>% 
   count(lat_dd_set, set_lat_deg, set_lat_dec,
         long_dd_set, set_long_deg, set_long_dec)
-
 
 # LORAN key
 loran_key <- data %>% 
@@ -185,11 +247,36 @@ usa <- rnaturalearth::ne_states(country="United States of America", returnclass 
 ggplot(data, aes(x=long_dd_set, y=lat_dd_set, color=port)) +
   geom_sf(data=usa, fill="grey85", color="white", inherit.aes = F) +
   geom_point() +
-  coord_sf(xlim=range(data$long_dd_set, na.rm=T),
-           ylim=range(data$lat_dd_set, na.rm=T)) +
+  # coord_sf(xlim=range(data$long_dd_set, na.rm=T),
+  #          ylim=range(data$lat_dd_set, na.rm=T)) +
+  coord_sf(xlim=c(-116, -128),
+           ylim=c(32, 44)) +
   theme_bw()
 
+# Comments
+freeR::uniq(data$comments1)
+freeR::uniq(data$comments2)
 
+
+
+# # LORAN-C conversions
+# ################################################################################
+# 
+# # Get LORAN-C Coordinates
+# loran_test <- wcfish::loran_to_gps(chain=9940,
+#                                    loran_x=loran_key$set_loran_x[1:30], 
+#                                    loran_w=loran_key$set_loran_w[1:30])
+# 
+# # Map results
+# usa <- rnaturalearth::ne_states(country="United States of America", returnclass = "sf")
+# ggplot(loran_test, aes(x=long_dd, y=lat_dd)) +
+#   geom_sf(data=usa, fill="grey85", color="white", inherit.aes = F) +
+#   geom_point() +
+#   coord_sf(xlim=range(loran_test$long_dd, na.rm=T),
+#            ylim=range(loran_test$lat_dd, na.rm=T)) +
+#   # coord_sf(xlim=c(-116, -128),
+#   #          ylim=c(32, 44)) +
+#   theme_bw()
 
 
 # Prep for export
@@ -198,7 +285,23 @@ ggplot(data, aes(x=long_dd_set, y=lat_dd_set, color=port)) +
 # Data out
 data_out <- data %>% 
   # Remove useless columns
-  select(-c(species_orig))
+  select(-c(species_orig, year_orig, duration_min_orig)) %>% 
+  # Remove useless lat/long columns
+  select(-c(set_lat_deg, set_lat_dec,
+            set_long_deg, set_long_dec,
+            up_lat_deg, up_lat_dec,
+            up_long_deg, up_long_dec)) %>%
+  # Remove these columns because they are empty
+  select(-c(set_loran_amin, set_loran_amax, up_loran_amin, up_loran_amax)) %>% 
+  # Move LORAN Y values into W column then remove Y columns
+  mutate(set_loran_w=ifelse(is.na(set_loran_w), set_loran_y, set_loran_w),
+         up_loran_w=ifelse(is.na(up_loran_w), up_loran_y, up_loran_w)) %>% 
+  select(-c(set_loran_y, up_loran_y))
+  
+
+# Inspect
+freeR::complete(data_out)
+
 
 # Export data
 ################################################################################
---
+++ code/shrimp_prawn_logbooks/Step2_plot_shrimp_prawn_logbooks.R
@@ -0,0 +1,438 @@
+
+
+# Read data
+################################################################################
+
+# Clear workspace
+rm(list = ls())
+
+# Packages
+library(tidyverse)
+
+# Directories
+indir <- "data/confidential/shrimp_prawn_logbooks/raw"
+outdir <- "data/confidential/shrimp_prawn_logbooks/processed"
+keydir <- "data/public/cdfw_keys/processed"
+plotdir <- "figures/shrimp_prawn_logbooks"
+
+# Read data
+data <- readRDS(file=file.path(outdir, "CDFW_1982_2023_shrimp_prawn_logbook_data.Rds"))
+blocks_sf <- wcfish::blocks
+
+
+# Themes
+################################################################################
+
+# Theme
+bar_theme <- theme(axis.text=element_text(size=7),
+                   axis.title=element_text(size=8),
+                   legend.text=element_text(size=7),
+                   legend.title=element_text(size=8),
+                   plot.tag=element_text(size=8),
+                   plot.subtitle = element_text(size=6, face="italic"),
+                   # Gridlines
+                   panel.grid.major = element_blank(), 
+                   panel.grid.minor = element_blank(),
+                   panel.background = element_blank(), 
+                   axis.line = element_line(colour = "black"),
+                   # Legend
+                   legend.background = element_rect(fill=alpha('blue', 0)))
+
+# Map theme
+map_theme <- theme(axis.text=element_text(size=7),
+                   axis.text.y = element_text(angle = 90, hjust = 0.5),
+                   axis.title=element_blank(),
+                   legend.text=element_text(size=6),
+                   legend.title=element_text(size=7),
+                   plot.tag=element_text(size=8),
+                   plot.subtitle = element_text(size=6, face="italic"),
+                   # Gridlines
+                   panel.grid.major = element_blank(), 
+                   panel.grid.minor = element_blank(),
+                   panel.background = element_blank(), 
+                   axis.line = element_line(colour = "black"),
+                   # Legend
+                   legend.background = element_rect(fill=alpha('blue', 0)))
+
+
+# Setup theme
+my_theme <-  theme(axis.text=element_text(size=7),
+                   axis.title=element_text(size=8),
+                   legend.text=element_text(size=7),
+                   legend.title=element_text(size=8),
+                   strip.text=element_text(size=8),
+                   plot.subtitle = element_text(size=6, face="italic"),
+                   plot.title=element_text(size=10),
+                   plot.tag=element_text(size=9),
+                   # Gridlines
+                   panel.grid.major = element_blank(), 
+                   panel.grid.minor = element_blank(),
+                   panel.background = element_blank(), 
+                   axis.line = element_line(colour = "black"),
+                   # Legend
+                   legend.background = element_rect(fill=alpha('blue', 0)))
+
+
+# Plot completeness
+################################################################################
+
+# Percent NAs
+n_nas <- freeR::complete(data)
+p_nas <- 1-n_nas/nrow(data)
+
+# Build data
+p_nas_df <- tibble(column=colnames(data),
+                   prop=p_nas) %>% 
+  # Remove uninteresting columns
+  filter(!column %in% c("year_orig", "year", "time_set_num", "time_up_num")) %>% 
+  # Mark spatial/not-spatial
+  mutate(type=ifelse(grepl("lat|long|loran", column), "Spatial", "Non-spatial")) %>% 
+  # Format columns
+  mutate(column=gsub("_", " ", column) %>% stringr::str_to_sentence()) %>%
+  mutate(column=recode(column,
+                       "Vessel"="Vessel name",
+                       "Spp code"="Species id",
+                       "Port code"="Port id",
+                       "Date tow"="Date of tow",
+                       "Date landing"="Date of landing",
+                       "Date depart"="Date of departure",
+                       "Depth fa set"="Start depth (fa)",
+                       "Depth fa up"="End depth (fa)",
+                       "Duration min"="Duration (min)",
+                       "Time set"="Start time",
+                       "Time up"="End time",
+                       "Headrope ft"="Head rope length (ft)",
+                       "Catch lbs"="Catch (lbs)",
+                       "Comments1"="Comments 1",
+                       "Comments2"="Comments 2",
+                       # Spatial
+                       "Long dd set"="Start longitude (°W)",
+                       "Lat dd set"="Start latitude (°N)",
+                       "Long dd up"="End longitude (°W)",
+                       "Lat dd up"="End latitude (°N)",
+                       "Set loran y"="Start LORAN-C Y-value",
+                       "Set loran x"="Start LORAN-C X-value",
+                       "Set loran w"="Start LORAN-C W-value",
+                       # "Up loran y"="End LORAN-C Y-value",
+                       "Up loran x"="End LORAN-C X-value",
+                       "Up loran w"="End LORAN-C W-value",
+                       # "Up loran cy"="End LORAN-C Y-value",
+                       "Up loran cx"="End LORAN-C X-value",
+                       "Up loran cw"="End LORAN-C W-value",
+                       # "Set loran amin"="Start LORAN-A min",
+                       # "Set loran amax"="Start LORAN-A max",
+                       # "Up loran amin"="End LORAN-A min",
+                       # "Up loran amax"="End LORAN-A max",
+  )) %>%
+  # Color
+  mutate(complete_yn=prop==1)
+
+# Plot data
+g <- ggplot(p_nas_df, aes(x=prop, y=reorder(column, desc(prop)), fill=complete_yn)) +
+  facet_grid(type~., scales="free_y", space="free_y") +
+  geom_bar(stat="identity") +
+  # Labels
+  labs(x="% complete", y="") +
+  scale_x_continuous(labels=scales::percent) +
+  scale_fill_discrete(name="100% complete?", guide=guide_legend(reverse=T)) +
+  # Theme
+  theme_bw() + my_theme +
+  theme(legend.position = "bottom")
+g
+
+# Export
+ggsave(g, filename=file.path(plotdir, "shrimp_prawn_logbook_completeness.png"), 
+       width=5.5, height=5.5, units="in", dpi=600)
+
+
+# Number of logbooks through time
+################################################################################
+
+# Stats
+stats <- data %>% 
+  group_by(year) %>% 
+  summarize(nlogs=n_distinct(logbook_id)) %>% 
+  ungroup()
+
+# Plot
+# 2008, 2011, 2012 no data
+g <- ggplot(stats, aes(x=year, y=nlogs)) +
+  geom_bar(stat="identity") +
+  # Labels
+  labs(x="Year", y="Number of logbooks") +
+  # Theme
+  theme_bw() + my_theme
+g
+
+# Export
+ggsave(g, filename=file.path(plotdir, "shrimp_prawn_logbook_nlogs.png"), 
+       width=5.5, height=3.5, units="in", dpi=600)
+
+
+# Depth
+################################################################################
+
+# Depth, set
+range(data$depth_fa_set, na.rm=T)
+g1 <- ggplot(data, aes(x=depth_fa_set)) +
+  geom_histogram(breaks=seq(0,910,1)) + 
+  # Limits
+  scale_x_continuous(lim=c(0, 300),
+                     breaks=seq(0, 300, 25),
+                     labels=c(breaks=seq(0, 275, 25), "≥300")) +
+  # Labels
+  labs(x="Start depth (fathoms)", y="# of logbook entries", tag="A") +
+  # Theme
+  theme_bw() + my_theme +
+  theme(axis.text.y = element_text(angle = 90, hjust = 0.5))
+g1
+
+# Depth, haul
+range(data$depth_fa_set, na.rm=T)
+g2 <- ggplot(data, aes(x=depth_fa_up)) +
+  geom_histogram(breaks=seq(0,910,1)) + 
+  # Limits
+  scale_x_continuous(lim=c(0, 300),
+                     breaks=seq(0, 300, 25),
+                     labels=c(breaks=seq(0, 275, 25), "≥300")) +
+  # Labels
+  labs(x="End depth (fathoms)", y="# of logbook entries", tag="B") +
+  # Theme
+  theme_bw() + my_theme +
+  theme(axis.text.y = element_text(angle = 90, hjust = 0.5))
+g2
+
+# Merge
+g <- gridExtra::grid.arrange(g1, g2, nrow=1)
+g
+
+# Export
+ggsave(g, filename=file.path(plotdir, "shrimp_prawn_logbook_depth_dists.png"), 
+       width=6.5, height=3, units="in", dpi=600)
+
+
+# Duration
+################################################################################
+
+# Plot start/end times
+tdata <- data %>%
+  select(logbook_id, time_set_num, time_up_num) %>%
+  gather(key="type", value="time", 2:ncol(.)) %>%
+  mutate(type=recode(type,
+                     "time_set_num"="Start",
+                     "time_up_num"="End"))
+
+# Plot times
+g1 <- ggplot(tdata, aes(x=time, fill=type)) +
+  geom_density(alpha=0.5) +
+  # Labels
+  labs(x="Time of day", y="Density", tag="A") +
+  scale_x_continuous(breaks=seq(0,24,4)) +
+  scale_fill_discrete(name="") +
+  # Theme
+  theme_bw() + my_theme +
+  theme(legend.position=c(0.8, 0.8),
+        legend.key.size=unit(0.3, "cm"),
+        axis.text.y = element_text(angle = 90, hjust = 0.5))
+g1
+
+# Plot duration
+range(data$duration_min/60, na.rm=T)
+g2 <- ggplot(data, aes(x=duration_min/60)) +
+  geom_histogram(breaks=seq(0,20,0.25)) +
+  # Labels
+  labs(y="# of logbook entries", x="Duration (hours)", tag="B") +
+  # Theme
+  theme_bw() + my_theme +
+  theme(axis.text.y = element_text(angle = 90, hjust = 0.5))
+g2
+
+# Merge
+g <- gridExtra::grid.arrange(g1, g2, nrow=1)
+g
+
+ggsave(g, filename=file.path(plotdir, "shrimp_prawn_logbook_duration_dists.png"),
+       width=6.5, height=3, units="in", dpi=600)
+
+
+# Catch
+################################################################################
+
+# Catch stats
+stats <- data %>% 
+  filter(!is.na(species) & !is.na(catch_lbs)) %>% 
+  group_by(species) %>% 
+  summarize(nvessels=n_distinct(vessel_id),
+         catch_lbs=median(catch_lbs)) %>% 
+  ungroup() %>% 
+  filter(nvessels>=3) %>% 
+  arrange(desc(catch_lbs))
+
+# Prep catch data
+cdata <- data %>% 
+  filter(species %in% stats$species) %>% 
+  mutate(species=factor(species, levels=stats$species))
+
+# Plot catch
+range(data$catch_lbs, na.rm=T)
+g <- ggplot(cdata, aes(x=catch_lbs, y=species)) +
+  geom_boxplot(outlier.shape = NA) +
+  # Labels
+  labs(x="Catch (lbs)", y="", subtitle = "Outliers and a few rare species are not shown to comply with the rule-of-three") +
+  scale_x_continuous(trans="log10") +
+  # Scale
+  theme_bw() + my_theme 
+g
+
+
+ggsave(g, filename=file.path(plotdir, "shrimp_prawn_logbook_catch_dists.png"), 
+       width=5.5, height=5, units="in", dpi=600)
+
+# Block
+################################################################################
+
+# Get land
+usa <- rnaturalearth::ne_states(country="United States of America", returnclass = "sf")
+foreign <- rnaturalearth::ne_countries(country=c("Canada", "Mexico"), returnclass = "sf")
+
+# Build stats
+stats <- data %>% 
+  # Species of interest
+  filter(species %in% c("Ridgeback prawn", "Spot prawn", "Pacific pink shrimp")) %>% 
+  # Summarize
+  group_by(species, block_id) %>% 
+  summarize(nfishers=unique(vessel_id),
+            catch_lbs=sum(catch_lbs, na.rm=T)) %>% 
+  ungroup() %>% 
+  # RUler of three
+  filter(nfishers>=3) %>% 
+  # Remove unknown species
+  filter(!is.na(species)) %>% 
+  # Spatialize
+  filter(!is.na(block_id)) %>% 
+  left_join(blocks_sf %>% select(block_id, geometry), by="block_id") %>% 
+  sf::st_as_sf()
+
+# Plot data
+g <- ggplot() +
+  # Facet
+  facet_wrap(~species, nrow=1) +
+  # Plot blocks
+  geom_sf(data=stats, mapping=aes(fill=catch_lbs),
+          color="grey30", lwd=0.1) +
+  # Plot land
+  geom_sf(data=foreign, fill="grey80", color="white", lwd=0.3) +
+  geom_sf(data=usa, fill="grey80", color="white", lwd=0.2) +
+  # Labels
+  labs(subtitle="A few rarely visited blocks are hidden to comply with rule-of-three") +
+  # Legend
+  scale_fill_gradientn(name="Catch (lbs)", 
+                       trans="log10",
+                       breaks=10^c(0:6),
+                       labels=parse(text=paste0("10^", 0:6)),
+                       colors=RColorBrewer::brewer.pal(9, "YlOrRd")) +
+  guides(fill = guide_colorbar(ticks.colour = "black", frame.colour = "black", frame.linewidth = 0.2)) +
+  # Crop
+  coord_sf(xlim = c(-126, -117), ylim = c(32.5, 42)) +
+  scale_x_continuous(breaks=seq(-124, -116, 4)) +
+  # Theme
+  theme_bw() + map_theme +
+  theme(legend.position = "bottom",
+        legend.key.size = unit(0.5, "cm"))
+g
+
+# Export
+ggsave(g, filename=file.path(plotdir, "shrimp_prawn_logbooks_block_catch.png"), 
+       width=6.5, height=4, units="in", dpi=600)
+
+
+# GPS data coverage
+################################################################################
+
+# GPS stats
+gps_stats <- data %>% 
+  # Summarize
+  group_by(year) %>% 
+  summarise(n=n(),
+            n_with_gps=sum( ( !is.na(lat_dd_set) & !is.na(long_dd_set) )  |  ( !is.na(lat_dd_up) & !is.na(long_dd_up) ) ) )%>% 
+  ungroup() %>% 
+  # Calc prop
+  mutate(prop=n_with_gps/n)
+
+# Plot data
+g <- ggplot(gps_stats, aes(x=year, y=prop)) +
+  geom_bar(stat="identity") +
+  # Labels
+  labs(x="Year", y="Percent of logbook entries\nwith GPS coordinates") +
+  scale_x_continuous(breaks=seq(1980, 2025,5)) +
+  scale_y_continuous(labels=scales::percent_format()) +
+  # Theme
+  theme_bw() + my_theme
+g
+
+# Export
+ggsave(g, filename=file.path(plotdir, "shrimp_prawn_logbook_gps_coverage.png"), 
+       width=5.5, height=3.5, units="in", dpi=600)
+
+
+# Plot locations
+################################################################################
+
+# Get midpoint
+get_midpoint <- function(cut_label) {
+  mean(as.numeric(unlist(strsplit(gsub("\\(|\\)|\\[|\\]", "", as.character(cut_label)), ","))))
+}
+
+# Breaks and midpoints
+lat_breaks <- seq(32, 42, 0.25)
+lat_mids <- zoo::rollmean(lat_breaks, k=2)
+long_breaks <- seq(-126,-114, 0.25)
+long_mids <- zoo::rollmean(long_breaks, k=2)
+
+# XY bins
+xy_ras_set <- data %>% 
+  # Reduce to logboooks with XY
+  filter(!is.na(lat_dd_set) & !is.na(long_dd_set)) %>% 
+  # Add bins for XY
+  mutate(lat_dd_bin=cut(lat_dd_set, breaks=lat_breaks, labels=lat_mids) %>% as.character() %>%  as.numeric(),
+         long_dd_bin=cut(long_dd_set, breaks=long_breaks, labels=long_mids) %>% as.character() %>% as.numeric()) %>% 
+  # Summarize
+  group_by(lat_dd_bin, long_dd_bin) %>% 
+  summarize(nvessels=n_distinct(vessel_id),
+            nlogbooks=n_distinct(logbook_id)) %>% 
+  ungroup() %>% 
+  # Rule of three
+  filter(nvessels>=3)
+
+# Get land
+usa <- rnaturalearth::ne_states(country="United States of America", returnclass = "sf")
+foreign <- rnaturalearth::ne_countries(country=c("Canada", "Mexico"), returnclass = "sf")
+
+# Plot
+g1 <- ggplot() +
+  # Plot land
+  geom_sf(data=foreign, fill="grey80", color="white", lwd=0.3) +
+  geom_sf(data=usa, fill="grey80", color="white", lwd=0.2) +
+  # Labels
+  labs(subtitle="Coordinates are summarized in bins to comply with the rule-of-three") +
+  # Plot points
+  geom_tile(data=xy_ras_set, mapping=aes(x=long_dd_bin, y=lat_dd_bin, fill=nlogbooks), alpha=0.9) +
+  # Crop
+  coord_sf(xlim = c(-126, -117), ylim = c(32, 42)) +
+  # Legend
+  scale_fill_gradientn(name="Number of logbooks", 
+                       colors=RColorBrewer::brewer.pal(9, "YlOrRd"), trans="log10",
+                       breaks=c(1, 5, 10, 25, 50, 100, 250, 500, 1000,  5000)) +
+  guides(fill = guide_colorbar(ticks.colour = "black", frame.colour = "black", frame.linewidth = 0.2)) +
+  # Theme
+  theme_bw() + my_theme +
+  theme(axis.title=element_blank(),
+        axis.text.y = element_text(angle = 90, hjust = 0.5),
+        legend.position = c(0.25, 0.2),
+        legend.key.size = unit(0.4, "cm"))
+g1
+
+# Export
+ggsave(g1, filename=file.path(plotdir, "shrimp_prawn_logbook_map.png"),
+       width=3.5, height=4.75, units="in", dpi=600)
+
